e0c8ab2b1e2b3b4fa37a8dd4cb80cb00c4d0408b
Amanda <amanda-m23@hotmail.com>
2017-03-29 11:58:30 -0400

    Practice files for learn python the hard way

---
+++ ex3.py
@@ -0,0 +1,24 @@
+#print statement
+print "I will now count my chickens:"
+#adds up hens and roosters
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+#statement of counting eggs
+print "Now I will count the eggs:"
+#adding up the eggs
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#posing a question
+print 'Is it true that 3 + 2 < 5 - 7'
+#mathematical equation
+print 3 + 2 < 5 - 7
+#adding and subtracting
+print 'What is 3 + 2?', 3 + 2
+print 'What is 5 - 7?', 5 - 7
+#statement
+print "Oh, that's why it's false."
+#statement
+print "How about some more."
+#more equations
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
