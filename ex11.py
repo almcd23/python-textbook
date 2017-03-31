89b52d9fc3a4f0d165bbb99b9efaf48a63c013eb
Amanda <amanda-m23@hotmail.com>
2017-03-31 13:44:43 -0400

    Exercise 11-13

---
+++ ex11.py
@@ -0,0 +1,16 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+
+print "Where do you live?"
+home = raw_input()
+print "What are you doing?"
+action = raw_input()
+
+print "So you live %r and you are doing %r." % (home, action)
